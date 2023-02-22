1c236f6693c5a1cfc9f75c75ee28b9b15e60b283
Rishi Patel <riishipatel.2.1@gmail.com>
2023-02-22 15:56:35 +0530

    image added

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
+const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
 
+        fileReader.onload = () => {
+            resolve(fileReader.result);
+        };
 
-
-
-
-
-
-
-function readformData(){
+        fileReader.onerror = (error) => {
+            reject(error);
+        };
+    });
+};
+async function readformData(){
+    let array = JSON.parse(localStorage.getItem("data")) ?? [];
     formData = {};
-    formData['Productname'] = document.getElementById('productName').value
-    formData['ProductPrice'] = document.getElementById('productprice').value
-    formData['Image'] = document.getElementById('Image').value
+    formData['ProductName'] = document.getElementById('productName').value
+    formData['ProductPrice'] = document.getElementById('productPrice').value
+    formData['Image'] = await convertBase64(document.getElementById('Image').files[0]);
     formData['description'] = document.getElementById('description').value
-
-    return formData;
+    console.log(formData)
+    array.push(formData)
+    localStorage.setItem("data", JSON.stringify(array));
+    displayData();
+}
+window.onload = displayData();
+function displayData(){
+    let obj = JSON.parse(localStorage.getItem("data"));
+    let data = '';
+    obj.forEach(product =>{
+        data += ` <div class="card m-2" style="width: 18rem;">
+        <img class="card-img-top" style="width:10rem; height:10rem; margin:auto;" src="${product.Image}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title Card-text">${product.ProductName}</h5>
+          <p class="card-text Card-text">${product.ProductPrice}</p>
+          <p class="card-text Card-text">${product.description}</p>
+          <button class="btn-success btn ">Delete</button>
+          <button class="btn-danger btn">Update</button>
+        </div>
+      </div>`
+    })
+    document.getElementById('products-row').innerHTML = data;
 }
